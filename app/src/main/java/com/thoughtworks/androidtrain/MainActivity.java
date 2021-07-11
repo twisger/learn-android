994a14b8ef67b1ee9388f4e8af17ba7305ef3f1e
Flynn <li.fulin@foxmail.com>
2021-07-11 04:59:09 -0400

    add constraint-layout

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
@@ -2,13 +2,28 @@ package com.thoughtworks.androidtrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity(view);
+            }
+        });
     }
+    public void openNewActivity(View view) {
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
+    }
+
 }
