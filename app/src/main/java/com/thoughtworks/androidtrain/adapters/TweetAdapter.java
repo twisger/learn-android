01925ef84e07049ca679cba0a8caaaf44002655a
Flynn <li.fulin@foxmail.com>
2021-07-11 05:03:20 -0400

    add multiple view type

--- app/src/main/java/com/thoughtworks/androidtrain/adapters/TweetAdapter.java
+++ app/src/main/java/com/thoughtworks/androidtrain/adapters/TweetAdapter.java
@@ -17,7 +17,8 @@ import java.util.List;
 
 public class TweetAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<Tweet> tweets = new ArrayList<>();
-
+    public static final int TweetItemType = 0;
+    public static final int EndTipType = 1;
     public void setTweets(List<Tweet> tweets) {
         this.tweets = tweets;
         notifyDataSetChanged();
@@ -26,19 +27,39 @@ public class TweetAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater from = LayoutInflater.from(parent.getContext());
-        return new ViewHolder(from.inflate(R.layout.tweet_item_layout, parent, false));
+
+        switch (viewType) {
+            case TweetItemType:
+                return new ViewHolder(from.inflate(R.layout.tweet_item_layout, parent, false));
+            case EndTipType:
+                return new ViewHolder(from.inflate(R.layout.tweet_end_tip_layout, parent, false));
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return EndTipType;
+        }
+        return TweetItemType;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ViewHolder tweetHolder = (ViewHolder)(holder);
-        tweetHolder.nick.setText(tweets.get(position).getSender().getNick());
-        tweetHolder.content.setText(tweets.get(position).getContent());
+        switch (getItemViewType(position)) {
+            case TweetItemType:
+                ViewHolder tweetHolder = (ViewHolder)(holder);
+                tweetHolder.nick.setText(tweets.get(position).getSender().getNick());
+                tweetHolder.content.setText(tweets.get(position).getContent());
+            case EndTipType:
+        }
+
     }
 
     @Override
     public int getItemCount() {
-        return tweets.size();
+        return tweets.size() + 1;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
