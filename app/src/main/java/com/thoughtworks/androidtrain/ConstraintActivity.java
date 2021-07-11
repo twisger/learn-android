a9cbeb9e1d029821a67959ec62eeff8e439e840a
Flynn <li.fulin@foxmail.com>
2021-07-11 05:00:43 -0400

    add constraint-layout

---
+++ app/src/main/java/com/thoughtworks/androidtrain/ConstraintActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.androidtrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
